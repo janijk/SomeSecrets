d90c3f0890b5aa31e4d4b1dc0f0c2601b83637b1
unknown <jani45@gmail.com>
2022-10-09 13:46:59 +0300

    FEAT: PasswordListItem

--- src/components/PasswordList.js
+++ src/components/PasswordList.js
@@ -1,62 +1,39 @@
-import { useState } from "react"
 import { FlatList, Text, View, Pressable, StyleSheet } from "react-native"
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { PasswordListItem } from '../components/PasswordListItem'
 
-export const PasswordList = ({ data }) => {
-    const itemHeader = () => (
-        <View style={styles.itemHeader}></View>
+/**
+ * Password list component, implements Flatlist.
+ * @param {*} credentials array of credential objects
+ * @returns 
+ */
+export const PasswordList = ({ data, longPress }) => {
+    const listHeader = () => (
+        <View style={styles.listHeader}>
+            <Text>List begins</Text>
+        </View>
     )
-    const itemFooter = () => (
-        <View style={styles.itemFooter}></View>
+    const listFooter = () => (
+        <View style={styles.listFooter}>
+            <Text>List ends</Text>
+        </View>
     )
     const itemSeparator = () => (
-        <View style={styles.seprator}></View>
+        <View style={styles.itemSeprator}></View>
     )
     const listEmpty = () => (
         <View><Text>No saved passwords</Text></View>
     )
 
-    const ListItem = ({ props }) => {
-        const [expanded, setExpanded] = useState(false);
-        return (
-            <View>
-                <View style={styles.itemViewHeader}>
-                    <Pressable style={styles.listItemPressable} onPress={() => setExpanded(!expanded)}>
-                        <Text style={styles.listItemProvider}>{props.provider}</Text>
-                        <Ionicons name={!expanded ? "md-chevron-down" : "md-chevron-up"} size={32} color="green" />
-                    </Pressable>
-                </View>
-                {expanded ?
-                    <>
-                        <View style={styles.itemView}>
-                            <Text style={styles.listItemText}>{`Username:   ${props.username}`}</Text>
-                            <Pressable>
-                                <Ionicons name="md-copy-outline" size={25} color="green" />
-                            </Pressable>
-                        </View>
-                        <View style={styles.itemView}>
-                            <Text style={styles.listItemText}>{`Password:    ${props.password}`}</Text>
-                            <Pressable>
-                                <Ionicons name="md-copy-outline" size={25} color="green" />
-                            </Pressable>
-                        </View>
-                    </>
-                    : null
-                }
-            </View>
-        )
-    }
-
     return (
         <View style={styles.container}>
             <FlatList
                 data={data}
-                renderItem={({ item }) => <ListItem props={item} />}
+                renderItem={({ item, index }) => <PasswordListItem props={item} longPress={() => longPress(index)} />}
                 keyExtractor={(item, i) => item.password + i}
                 ItemSeparatorComponent={itemSeparator}
                 ListEmptyComponent={listEmpty}
-                ListHeaderComponent={itemHeader}
-                ListFooterComponent={itemFooter}
+                ListHeaderComponent={listHeader}
+                ListFooterComponent={listFooter}
             />
         </View>
     )
@@ -66,33 +43,18 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "lightgrey",
     },
-    seprator: {
+    itemSeprator: {
         height: 1,
         marginBottom: 5,
-        backgroundColor: "white",// '#03095e',
+        backgroundColor: "white",
         opacity: 0.5
     },
-    listItemProvider: {
-        fontWeight: "bold",
-        fontSize: 20,
+    listHeader: {
+        height: 25,
+        alignItems: "center"
     },
-    listItemText: {
-        fontSize: 15,
-    },
-    itemView: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        backgroundColor: "whitesmoke"
-        // borderWidth: 1, borderColor: "black"
-    },
-    itemViewHeader: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    listItemPressable: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
+    listFooter: {
+        height: 25,
         alignItems: "center"
     }
 })
--- src/screens/PasswordListScreen.js
+++ src/screens/PasswordListScreen.js
@@ -1,7 +1,6 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { PasswordList } from '../components/PasswordList';
 
-
 export const PasswordListScreen = () => {
     const data = [
         {
@@ -35,12 +34,27 @@ export const PasswordListScreen = () => {
             password: 'bd7acbea-c1b1',
         },
     ];
-
+    // Handles PasswordListItem Pressable onLongPress. indx param is the index of
+    // specific credential object in credential array.
+    const handleLongPress = (indx) => {
+        // User actions on specific credential entry edit/delete 
+        console.log(indx);
+    }
+    // Sort credential to ascending order by provider name
+    (sortAscending = () => {
+        data.sort((a, b) => {
+            let fa = a.provider.toLowerCase(),
+                fb = b.provider.toLowerCase();
+            if (fa < fb) return -1;
+            if (fa > fb) return 1;
+            return 0;
+        });
+    })();
     return (
         <>
             <Text>this is PasswordListScreen</Text>
             <View>
-                <PasswordList data={data} />
+                <PasswordList data={data} longPress={(indx) => handleLongPress(indx)} />
             </View>
         </>
     )
